930d009da26d40bf6b46214803f9b1f3b8a10355
Bas Vodde <basv@odd-e.com>
2014-07-10 19:44:04 +0800

    Refactored the code a lot

--- lib/discover_tabs.rb
+++ lib/discover_tabs.rb
@@ -3,43 +3,45 @@ require 'ptools'
 
 module DiscoverTabs
 
-  def self.in_dir(dir, pattern = "*")
-    files_with_tabs = false
-    Dir["#{dir}/**/#{pattern}"].each do |file|
-      begin
-        has_tabs = false
-        unless File.directory?(file) || File.binary?(file)
-          has_tabs = in_file(file)
-        end
-      rescue ArgumentError
-      end
-      files_with_tabs ||= has_tabs
+  def self.files_with_tab_indenting(full_path)
+    filename = File.basename(full_path)
+    dirname = File.dirname(full_path)
+
+    files_with_tabs = []
+    if File.directory?(full_path)
+      files_with_tabs += files_with_tab_indenting_in_dir(full_path)
+    elsif filename.include?("*")
+      files_with_tabs += files_with_tab_indenting_in_dir(dirname, filename)
+    else
+      files_with_tabs << full_path if file_has_tabs_indenting?(full_path)
     end
     files_with_tabs
   end
 
-  def self.in_file(filename)
-    File.readlines(filename).each do |line|
-      if line =~ /^\t+/
-        puts Pathname.new(filename).cleanpath.to_s
-        return true
+  def self.files_with_tab_indenting_in_dir(dir, pattern = "*")
+    files_with_tabs = []
+    Dir["#{dir}/**/#{pattern}"].each do |file|
+      begin
+        unless File.directory?(file) || File.binary?(file)
+          files_with_tabs << file if file_has_tabs_indenting?(file)
+        end
+      rescue ArgumentError
       end
+    end
+    files_with_tabs
+  end
+
+  def self.file_has_tabs_indenting?(filename)
+    File.readlines(filename).each do |line|
+      return true if line =~ /^\t+/
     end
     false
   end
 
   def self.cmdline_run(argv)
     full_path = argv[0] || "."
-    filename = File.basename(full_path)
-    dirname = File.dirname(full_path)
-
-    found_tabs = if File.directory?(full_path)
-      in_dir(full_path)
-    elsif filename.include?("*")
-       in_dir(dirname, filename)
-    else
-      in_file(full_path)
-    end
-    found_tabs ? 0 : 1
+    files_with_tabs = files_with_tab_indenting(full_path)
+    files_with_tabs.each { |file| puts Pathname.new(file).cleanpath.to_s }
+    files_with_tabs.empty? ? 1 : 0
   end
 end
--- spec/discover_tabs_spec.rb
+++ spec/discover_tabs_spec.rb
@@ -2,67 +2,74 @@ require 'spec_helper.rb'
 
 describe "It can discover tabs" do
 
-  it "Can discover tabs in one file" do
-    DiscoverTabs.should_receive(:puts).with(/file_with_tabs.rb/)
-    DiscoverTabs.in_file(test_data("file_with_tabs.rb")).should== true
-  end
+  context "Discovering files with tab indentation" do
 
-  it "Won't discover tabs in one file without tabs" do
-    DiscoverTabs.should_not_receive(:puts)
-    DiscoverTabs.in_file(test_data("file_without_tabs.rb")).should== false
-  end
+    it "Can discover tabs in one file" do
+      DiscoverTabs.file_has_tabs_indenting?(test_data("file_with_tabs.rb")).should== true
+    end
 
-  it "Won't discover any files with tabs in a directory when there are none" do
-    DiscoverTabs.should_not_receive(:puts)
-    DiscoverTabs.in_dir(test_data("dir_without_tabbled_files")).should== false
-  end
+    it "Won't discover tabs in one file without tabs" do
+      DiscoverTabs.file_has_tabs_indenting?(test_data("file_without_tabs.rb")).should== false
+    end
 
-  it "Won't discover any files with tabs in a directory when there are none" do
-    DiscoverTabs.should_receive(:puts).with(/dir_with_some_tabbed_files\/1.rb/)
-    DiscoverTabs.should_receive(:puts).with(/dir_with_some_tabbed_files\/8\/1.rb/)
-    DiscoverTabs.should_receive(:puts).with(/dir_with_some_tabbed_files\/dir\/4.rb/)
-    DiscoverTabs.in_dir(test_data("dir_with_some_tabbed_files")).should== true
-  end
+    it "Won't discover any files with tabs in a directory when there are none" do
+      DiscoverTabs.files_with_tab_indenting_in_dir(test_data("dir_without_tabbled_files")).should be_empty
+    end
 
-  it "Can deal with the current dir" do
-    DiscoverTabs.should_receive(:puts).at_least(1).times
-    DiscoverTabs.in_dir(".").should== true
-  end
+    it "Won't discover any files with tabs in a directory when there are none" do
+      files_with_tabs = DiscoverTabs.files_with_tab_indenting_in_dir(test_data("dir_with_some_tabbed_files"))
+      files_with_tabs[0].should== test_data("dir_with_some_tabbed_files/1.rb")
+      files_with_tabs[1].should== test_data("dir_with_some_tabbed_files/8/1.rb")
+      files_with_tabs[2].should== test_data("dir_with_some_tabbed_files/dir/4.rb")
+      files_with_tabs.size.should== 3
+    end
 
-  it "Should swallow ArgumentError expections" do
-    DiscoverTabs.stub(:in_file).and_raise(ArgumentError)
-    DiscoverTabs.in_dir(".").should== false
-  end
+    it "Can deal with the current dir" do
+      DiscoverTabs.files_with_tab_indenting_in_dir(".").should_not be_empty
+    end
 
-  it "Makes sure the pathname is clean" do
-    DiscoverTabs.should_receive(:puts).with(/dir_with_some_tabbed_files_and_different_extensions\/tabs.rb/)
-    DiscoverTabs.should_receive(:puts)
-    DiscoverTabs.in_dir(test_data("dir_with_some_tabbed_files_and_different_extensions/")).should== true
-  end
+    it "Should swallow ArgumentError expections" do
+      DiscoverTabs.stub(:file_has_tabs_indenting?).and_raise(ArgumentError)
+      DiscoverTabs.files_with_tab_indenting_in_dir(".").should== []
+    end
 
-  it "will only find the files with a certain extension" do
-    DiscoverTabs.should_receive(:puts).with(/dir_with_some_tabbed_files_and_different_extensions\/tabs.rb/)
-    DiscoverTabs.should_not_receive(:puts).with(/dir_with_some_tabbed_files_and_different_extensions\tabs.cpp/)
-    DiscoverTabs.in_dir(test_data("dir_with_some_tabbed_files_and_different_extensions"), "*.rb").should== true
-  end
+    it "will only find the files with a certain extension" do
+      files_with_tabs = DiscoverTabs.files_with_tab_indenting_in_dir(test_data("dir_with_some_tabbed_files_and_different_extensions"), "*rb")
+      files_with_tabs.size.should== 1
+      files_with_tabs[0].should== test_data("dir_with_some_tabbed_files_and_different_extensions/tabs.rb")
+    end
 
-  it "will discover tabs in a file when a filename it passed (success)" do
-    DiscoverTabs.should_receive(:in_file).with(test_data("file_with_tabs.rb")).and_return(true)
-    DiscoverTabs.cmdline_run([ test_data("file_with_tabs.rb") ]).should== 0
-  end
+    it "will discover tabs in a file when a filename it passed (success)" do
+      DiscoverTabs.should_receive(:file_has_tabs_indenting?).with(test_data("file_with_tabs.rb")).and_return(true)
+      DiscoverTabs.files_with_tab_indenting(test_data("file_with_tabs.rb")).should== [test_data("file_with_tabs.rb")]
+    end
 
-  it "will discover tabs in a dir when a directory it passed (failed)" do
-    DiscoverTabs.should_receive(:in_dir).with(test_data("dir_with_some_tabbed_files")).and_return(false)
-    DiscoverTabs.cmdline_run([ test_data("dir_with_some_tabbed_files") ]).should== 1
-  end
+    it "will discover tabs in a dir when a directory it passed (failed)" do
+      DiscoverTabs.should_receive(:files_with_tab_indenting_in_dir).with(test_data("dir_with_some_tabbed_files")).and_return([])
+      DiscoverTabs.files_with_tab_indenting(test_data("dir_with_some_tabbed_files")).should== []
+    end
 
-  it "will discover a pattern (based on having an * in the filename) and it passes the pattern (success)" do
-    DiscoverTabs.should_receive(:in_dir).with(test_data("dir_with_some_tabbed_files"), "*").and_return(true)
-    DiscoverTabs.cmdline_run([ test_data("dir_with_some_tabbed_files/*") ]).should== 0
+    it "will discover a pattern (based on having an * in the filename) and it passes the pattern (success)" do
+      DiscoverTabs.should_receive(:files_with_tab_indenting_in_dir).with(test_data("dir_with_some_tabbed_files"), "*").and_return(["file"])
+      DiscoverTabs.files_with_tab_indenting(test_data("dir_with_some_tabbed_files/*")).should== ["file"]
+    end
   end
 
   it "Defaults to the current dir" do
-    DiscoverTabs.should_receive(:in_dir).with(".").and_return(true)
-    DiscoverTabs.cmdline_run([])
+    DiscoverTabs.should_receive(:files_with_tab_indenting).with(".").and_return([])
+    DiscoverTabs.cmdline_run([]).should== 1
   end
+
+  it "Should print all the file to stdout" do
+    DiscoverTabs.should_receive(:files_with_tab_indenting).with("file").and_return(["file"])
+    DiscoverTabs.should_receive(:puts).with("file")
+    DiscoverTabs.cmdline_run(["file"]).should== 0
+  end
+
+  it "Makes sure the pathname is clean when printing a file" do
+    DiscoverTabs.should_receive(:files_with_tab_indenting).with("dir//file").and_return(["dir//file"])
+    DiscoverTabs.should_receive(:puts).with("dir/file")
+    DiscoverTabs.cmdline_run(["dir//file"]).should== 0
+  end
+
 end
